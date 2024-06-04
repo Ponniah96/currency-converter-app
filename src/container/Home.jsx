70dda5e60e89e38a6bf57ace747ba82d0465b924
Ponniah Kothandaraman (DevOn) <p.kothandaraman@devon.nl>
2024-06-05 00:46:44 +0530

    Password Generator HomeScreen Content Update

--- src/container/Home.jsx
+++ src/container/Home.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import "../styles/card.scss";
 import currencyConverter from "../data/images/currency-converter.png";
+import passwordGenerator from "../data/images/password-generator.png";
 
 export function Home() {
   return (
@@ -14,7 +15,8 @@ export function Home() {
             value into another country value
           </div>
           <div>
-            Concepts: React hooks, Axios, Material UI, Text to speech conversion
+            <b className="p-0">Concepts: </b>
+            React hooks, Axios, Material UI, Text to speech conversion
           </div>
         </div>
         <div className="card-section-footer">
@@ -24,7 +26,7 @@ export function Home() {
         </div>
       </div>
       <div className="card-section">
-        <img className="card-section-image" src={currencyConverter} alt="" />
+        <img className="card-section-image" src={passwordGenerator} alt="" />
         <div className="card-section-body">
           <div className="card-section-body-header">
             Random Password Generator
@@ -34,28 +36,12 @@ export function Home() {
             on parameters like length, numbers, uppercase, lowercase and
             numbers.
           </div>
-          <div>Card Description2</div>
+          <div>
+            <b className="p-0">Concepts:</b>React hooks, Javascript, SASS
+          </div>
         </div>
         <div className="card-section-footer">
           <Link to="/random-password-generator" className="primary-button">
-            Visit Application
-          </Link>
-        </div>
-      </div>
-      <div className="card-section">
-        <img className="card-section-image" src={currencyConverter} alt="" />
-        <div className="card-section-body">
-          <div className="card-section-body-header">Currency Converter</div>
-          <div>
-            Currency Converter is used to convert any currency from one country
-            value into another country value
-          </div>
-          <div>
-            Concepts: React hooks, Axios, Material UI, Text to speech conversion
-          </div>
-        </div>
-        <div className="card-section-footer">
-          <Link to="/currency-converter" className="primary-button">
             Visit Application
           </Link>
         </div>
